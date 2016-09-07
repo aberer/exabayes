6a3ee654828001df767bd3f6e42cd37a023ddf2b
Andre Aberer <andre.aberer@googlemail.com>
2016-09-07 21:24:46 +0200

    adds bootstrap script

--- utils/updateDate.sh
+++ utils/updateDate.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-date=$( date "+%F" )
 commitId=$(git log  | head -n 1  | cut -f 2  -d ' ' )
+date=$(git log --date=iso -1  | grep Date  | sed 's/Date:[ ]*\(.*\)/\1/' | cut -f 1,2 -d ' ')
 
 echo "#define RELEASE_DATE \"$date\"" > src/releaseDate.hpp
 echo "#define GIT_COMMIT_ID \"$commitId\"" >> src/releaseDate.hpp
