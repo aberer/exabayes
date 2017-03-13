c3238dc41dd1ae93eda401f1cbad1ebac3919bed
Andre Aberer <andre.aberer@googlemail.com>
2017-03-13 23:37:11 +0100

    enhances bootstrap to work without git checkout

--- utils/updateDate.sh
+++ utils/updateDate.sh
@@ -1,7 +1,20 @@
 #! /bin/bash
 
-commitId=$(git log  | head -n 1  | cut -f 2  -d ' ' )
-date=$(git log --date=iso -1  | grep Date  | sed 's/Date:[ ]*\(.*\)/\1/' | cut -f 1,2 -d ' ')
+root=$(dirname $(dirname $0))
+
+echo $root
+
+
+isGit=$?
+
+if [ -d $root/.git ]; then
+    commitId=$(git log  | head -n 1  | cut -f 2  -d ' ' )
+    date=$(git log --date=iso -1  | grep Date  | sed 's/Date:[ ]*\(.*\)/\1/' | cut -f 1,2 -d ' ')
+else
+    version=$(cat $root/VERSION)
+    commitId="-- not built from git (version: $version) --"
+    date="$(date +'%Y-%m-%d %H:%M:%S')"
+fi
 
 echo "#define RELEASE_DATE \"$date\"" > src/releaseDate.hpp
 echo "#define GIT_COMMIT_ID \"$commitId\"" >> src/releaseDate.hpp
@@ -9,5 +22,3 @@ echo "#define GIT_COMMIT_ID \"$commitId\"" >> src/releaseDate.hpp
 echo -e  "\n\nupdated release date: "
 cat src/releaseDate.hpp
 echo -e  "\n\n"
-
-
